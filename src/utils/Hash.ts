6da5e98384723c5ebf9eb3b2555944e39b359ac0
progerdav <developer.gyulnazaryan@gmail.com>
2021-11-06 21:00:18 +0400

    Upgrading to mysql2 client, in order to support more secure password.

--- src/utils/Hash.ts
+++ src/utils/Hash.ts
@@ -1,12 +1,12 @@
 import * as bcrypt from 'bcryptjs';
 
 export class Hash {
-  static make(plainText) {
+  static make(plainText: string) {
     const salt = bcrypt.genSaltSync(10);
     return bcrypt.hashSync(plainText, salt);
   }
 
-  static compare(plainText, hash) {
+  static compare(plainText: string, hash: string) {
     return bcrypt.compareSync(plainText, hash);
   }
 }
